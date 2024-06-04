dcfedcddf653ed8d82d318076eb6eba8be5da9f3
Francesco DAmore <fdamore@deloitte.it>
2024-06-04 13:01:15 +0000

    route-target definiti

---
+++ webapp/controller/BEMDetail.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/Label",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/comp/smartvariants/PersonalizableInfo",
+    "sap/ui/core/Fragment"
+    
+], function(Controller, JSONModel, Label, Filter, FilterOperator, PersonalizableInfo, Fragment) {
+    "use strict";
+
+    return Controller.extend("sap.m.bem.controller.BEMDetail", {
+        onInit: function() {   }
+
+    });
+});
